f47ee6666e6c8d4e827f4de384d67ba13d590b54
Mika <91550454+48222@users.noreply.github.com>
2025-09-12 08:52:41 +0100

    Update receive_message.py
    Customers cannot receive MTs as the example exits after acquiring signal. Need a loop, and any way waiting for signal is a bad practice that we do not and should not advise.

--- examples/python/receive_message.py
+++ examples/python/receive_message.py
@@ -6,7 +6,7 @@ from time import sleep
 #
 #A serial connection will first be attempted on the selected port, if successful 
 #the device information will be printed and the signal will be polled and once obtained 
-#the script will listen for a message to come in.
+#the script will listen for a message to come in. #not what I see happening.
 #
 #Requirements:
 #Have an open view of the sky where a good signal can be obtained.
@@ -19,7 +19,7 @@ if __name__ == '__main__':
     args = parser.parse_args()
 
     if (args.device):
-        # Create RockBlock9704 instance
+        # Create RockBLOCK9704 instance
         rb = RockBlock9704()
 
         # Begin serial communication
@@ -32,18 +32,30 @@ if __name__ == '__main__':
             print("Hardware version: \t", rb.get_hardware_version())
             print("9704 Modem IMEI: \t", rb.get_imei())
 
-            # Wait for signal
-            while rb.get_signal() < 1:
-                print("Waiting for signal...")
-                sleep(2)
+            # --- Extremely bad practice to wait for signal ---
+            # while rb.get_signal() < 1:
+            #     print("Waiting for signal...")
+            #     sleep(2)
+            #
+            # print("Signal acquired!")
 
-            print("Signal acquired!")
-
+            # --- premature exit resulting in signal acquired but no MT ---
             # Check for messages
-            message = rb.receive_message()
+            # message = rb.receive_message()
+            #
+            # if message is not None:
+            #     print("Received message: ", message)
 
-            if message is not None:
-                print("Received message: ", message)
+            # --- new loop waiting for MT ---
+            print("Listening for MT messages... Press Ctrl+C to stop.")
+            try:
+                while True:
+                    message = rb.receive_message()
+                    if message is not None:
+                        print("Received message: ", message)
+                    sleep(1)
+            except KeyboardInterrupt:
+                print("\nStopped by user.")
     else:
         print("Please specify a serial port")
         parser.print_help()
